626ba8ba127c6fb0e11281cb997b036735a8baf4
Theodore Baschak <theodore@ciscodude.net>
2015-12-30 21:06:34 -0600

    add traceroute to traceroute output

--- atlas-examine-traceroute-measurement.py
+++ atlas-examine-traceroute-measurement.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
       if data[index].has_key("dst_addr"):
         print "Dest:     ", data[index]["dst_addr"]
       print "Probe ID: ", data[index]["prb_id"]
+      print "Type:     ", data[index]["msm_name"]
       print "Proto:    ", data[index]["proto"]
 
       for resultindex in range(len(data[index]["result"])):
